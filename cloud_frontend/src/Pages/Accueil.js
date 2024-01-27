60f9945cd4dfa5f369949e73da88e511311a813f
hery007 <handrianimanana@gmail.com>
2024-01-28 00:41:13 +0300

    update assets and css

---
+++ cloud_frontend/src/Pages/Accueil.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Components/Header";
+import SideBar from "../Components/SideBar";
+
+function Accueil()
+{
+    return(
+        <>
+            <Header/>
+            <SideBar/>
+        </>
+        
+    );
+    
+}
+export default Accueil;
